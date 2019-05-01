89c02908f5f6d40ead63582811145fe949bbbb8e
yangsong <yangsong@yangsongdeMacBook-Pro.local>
2019-05-01 01:31:40 -0400

    message

---
+++ fast_ALS/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  fast_ALS
+//
+//  Created by yangsong on 5/1/19.
+//  Copyright © 2019 yangsong. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
